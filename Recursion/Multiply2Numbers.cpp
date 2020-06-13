14eab5054a6a3751316c58ce19215f2715cfa7e6
Sharannyo Basu <yobasu2015@gmail.com>
2020-06-14 02:44:44 +0530

    Create Multiply2Numbers.cpp

---
+++ Recursion/Multiply2Numbers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int issorted(int b, int n)
+{
+    if(n==0)
+    {
+        return 0;
+    }
+    return b+issorted(b,n-1);
+}
+int main()
+{
+    int b,n;
+    cin>>b>>n;
+    cout<<issorted(b,n);
+}
